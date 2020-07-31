7118e642c5aaf638ad17c2fc61575bd7a327708d
Gustavo Farache <gustavo.oliveira@sefaz.am.gov.br>
2020-07-31 13:39:01 -0400

    Classe de teste para escrita de valor por extenso

--- DengoDeCrianca/src/br/com/dengodecrianca/util/TestePorExtenso.java
+++ DengoDeCrianca/src/br/com/dengodecrianca/util/TestePorExtenso.java
@@ -7,10 +7,10 @@ public class TestePorExtenso {
 	public static void main(String[] args) {
 		CurrencyWriter cw = CurrencyWriter.getInstance();
 
-		String extenso = cw.write(new BigDecimal("234.89"));
+		String extenso = cw.write(new BigDecimal("7821234.89"));
 		System.out.println(extenso);
 
-		extenso = cw.write(new BigDecimal("130000000000"));
+		extenso = cw.write(new BigDecimal("130000000.34"));
 		System.out.println(extenso);
 	}
 
