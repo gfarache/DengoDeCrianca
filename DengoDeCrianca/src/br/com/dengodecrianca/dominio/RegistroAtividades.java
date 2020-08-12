2a55611be5d82ee7f35069898ae3ea440d7db75c
Gustavo Farache <gustavo.oliveira@sefaz.am.gov.br>
2020-08-12 10:15:58 -0400

    Criação de domínio para nova tabela de registro de atividades

---
+++ DengoDeCrianca/src/br/com/dengodecrianca/dominio/RegistroAtividades.java
@@ -0,0 +1,10 @@
+package br.com.dengodecrianca.dominio;
+
+public class RegistroAtividades {
+	private String ID = "";
+	private String data = "";
+	private String descricao = "";
+	private String login = "";
+	
+
+}
